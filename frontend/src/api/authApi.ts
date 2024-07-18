a662af001cab884b718e5e2e85d9c57ad02f950d
LongNguyen25012002 <25nguyenduclong@gmail.com>
2024-07-18 16:48:56 +0700

    fix value AUTH_URL

--- frontend/src/api/authApi.ts
+++ frontend/src/api/authApi.ts
@@ -1,7 +1,7 @@
 import { apiBase } from "@/httpIntercepter_";
 import { AuthGuard } from "./models/authGuard";
 
-const AUTH_URL: string = "http://localhost:3005/v1/auth";
+const AUTH_URL: string = "http://localhost:3000/api/v1/auth";
 const URL_LOGIN_GOOGLE: string = AUTH_URL + "/google";
 
 const reigster = (userPayload: AuthGuard) => {
