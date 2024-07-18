31045d2f64f9195b1f7a001a1affc638aae1661c
LongNguyen25012002 <25nguyenduclong@gmail.com>
2024-07-18 16:50:57 +0700

    export avatar componet

--- frontend/src/components/ui/index.ts
+++ frontend/src/components/ui/index.ts
@@ -2,3 +2,4 @@ export { default as Button } from "./button/Button";
 export { default as Grid } from "./grid/Grid";
 export { default as Image } from "./image/Image";
 export { default as Separate } from "./separate/Separate";
+export { default as Avatar } from "./avatar/Avatar";
