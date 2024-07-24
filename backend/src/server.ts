cd40d0d4d12a1a25bc29218833325caa01bad7ce
LongNguyen25012002 <25nguyenduclong@gmail.com>
2024-07-24 18:52:23 +0700

    added body parser and date-fns libraries

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,23 +1,19 @@
 import express, { Application, Request, Response } from 'express';
 import { connectDB } from './configs/database';
 import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
 import cors from 'cors';
 import passport from 'passport';
 import api from './routers/api';
-const app: Application = express();
 connectDB();
 
+const app: Application = express();
 app.use(express.json());
 app.use(passport.initialize());
 app.use(cookieParser());
-
-app.use(
-  cors({
-    origin: 'http://localhost:5174',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true
-  })
-);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
 app.use(api);
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
