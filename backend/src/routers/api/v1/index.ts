917c74734fb8eabdea2642c33e3aa3a6c053056a
LongNguyen25012002 <25nguyenduclong@gmail.com>
2024-07-24 19:05:36 +0700

    add route to index file

--- backend/src/routers/api/v1/index.ts
+++ backend/src/routers/api/v1/index.ts
@@ -1,7 +1,15 @@
 import express from 'express';
 import authRouters from './authRouters';
+import categoriesExpenseRouter from './categoriesExpenseRouter';
+import budgetRouters from './budgetRouters';
+import expenseRouters from './expenseRouters';
+import budgetCateRouters from './budgetCateRouters';
 
 const Router = express.Router();
 Router.use('/v1/auth', authRouters);
+Router.use('/v1', expenseRouters);
+Router.use('/v1', categoriesExpenseRouter);
+Router.use('/v1', budgetRouters);
+Router.use('/v1', budgetCateRouters);
 
 export default Router;
