2b3c8ef7b533a44c8d4b42da5a22b2a5462bc96b
LongNguyen25012002 <25nguyenduclong@gmail.com>
2024-07-24 19:02:33 +0700

    add type section for budgets

---
+++ backend/src/models/guard/budgets.ts
@@ -0,0 +1,27 @@
+import { Document, Types } from 'mongoose';
+import { SchemaTimestampsConfig } from './common';
+
+export interface MonthBudget {
+  amount: number;
+  budgetCategories: Types.ObjectId[];
+}
+
+export interface YearlyBudget {
+  amount: number;
+  amount_actually: number;
+  balance: number;
+  timestamps: SchemaTimestampsConfig;
+  month: {
+    [month: string]: MonthBudget;
+  };
+}
+
+export interface Budgets {
+  [year: string]: YearlyBudget;
+}
+
+export interface BudgetsDocument extends Document {
+  budgets: Budgets;
+}
+
+export default BudgetsDocument;
