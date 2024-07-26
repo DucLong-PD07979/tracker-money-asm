81df67accb294c6afe72bdd51c39e7560229ad65
LongNguyen25012002 <25nguyenduclong@gmail.com>
2024-07-26 22:22:50 +0700

    create feature add expenses

--- frontend/src/pages/profile/components/form/FormExpense.tsx
+++ frontend/src/pages/profile/components/form/FormExpense.tsx
@@ -9,54 +9,51 @@ import {
     InputText,
     InputDate,
     InputRadio,
+    ErrorMess,
 } from "@/components/ui/form/input";
 import { PlusIcon } from "@/components/ui/icon";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { getAllCategoriesExpense } from "@/api/categoryExpenseApi";
+import { ExpensesType } from "@/api/models/expensesGuard";
+import { createExpenses } from "@/api/expensesApi";
+import { toast } from "react-toastify";
 
-interface BudgetsOptionType {
-    value: string;
-    label: string;
-}
+// interface BudgetsOptionType {
+//     value: string;
+//     label: string;
+// }
 
-const _budgetsTypeOption: BudgetsOptionType[] = [
-    { value: "groceries", label: "Groceries" },
-    { value: "electricity", label: "Electricity" },
-    { value: "rent", label: "Rent" },
-    { value: "education", label: "Education" },
-    { value: "entertainment", label: "Entertainment" },
-    { value: "shopping", label: "Shopping" },
-    { value: "internet", label: "Internet" },
-    { value: "healthcare", label: "Healthcare" },
-    { value: "income", label: "Income" },
-    { value: "another", label: "Another" },
-];
+// const _budgetsTypeOption: BudgetsOptionType[] = [
+//     { value: "groceries", label: "Groceries" },
+//     { value: "electricity", label: "Electricity" },
+//     { value: "rent", label: "Rent" },
+//     { value: "education", label: "Education" },
+//     { value: "entertainment", label: "Entertainment" },
+//     { value: "shopping", label: "Shopping" },
+//     { value: "internet", label: "Internet" },
+//     { value: "healthcare", label: "Healthcare" },
+//     { value: "income", label: "Income" },
+//     { value: "another", label: "Another" },
+// ];
 
 interface CurrencyOptionType {
     value: string;
     label: string;
+    _id: number;
 }
 const _currencyOption: CurrencyOptionType[] = [
-    { value: "VND", label: "VND" },
-    { value: "USD", label: "USD" },
-    { value: "EUR", label: "EUR" },
+    { value: "VND", label: "VND", _id: 123 },
+    { value: "USD", label: "USD", _id: 123 },
+    { value: "EUR", label: "EUR", _id: 123 },
 ];
 
-interface FormBudgetsType {
-    budgetsOption: string;
-    amount: number;
-    dateStart: Date;
-    dateEnd: Date;
-    description: string;
-    currency: string;
-}
-// currency: mixed().oneOf(["VND", "USD", "EUR"]).required(),
-
-const budgetsSchema = object({
-    budgetsOption: string().required(),
+const expensesSchema = object({
+    id_expense_cate: string().required(),
     amount: number().required(),
-    dateStart: date().default(() => new Date()),
-    dateEnd: date().default(() => new Date()),
+    duration: date().default(() => new Date()),
+    // is_paid: boolean().default(false),
     description: string().default(""),
-    currency: string().default("VND").required(),
+    currency_code: string().default("VND").required(),
 });
 
 const FormExpense = () => {
@@ -65,34 +62,63 @@ const FormExpense = () => {
         handleSubmit,
         register,
         reset,
-        // formState: { errors },
-    } = useForm<FormBudgetsType>({
-        resolver: yupResolver(budgetsSchema),
+        formState: { errors },
+    } = useForm<ExpensesType>({
+        resolver: yupResolver(expensesSchema),
     });
 
-    const [budgetsOption, setBudgetsOption] =
-        useState<BudgetsOptionType[]>(_budgetsTypeOption);
     const [currencyOption, setCurrencyOption] =
         useState<CurrencyOptionType[]>(_currencyOption);
 
-    const handleCreateTypeBudgets = async () => {
-        setBudgetsOption(budgetsOption);
-        setCurrencyOption(currencyOption);
-        reset();
+    const { isPending, data: cateExpenseData } = useQuery({
+        queryKey: ["cate-expenses"],
+        queryFn: getAllCategoriesExpense,
+        staleTime: 5 * 1000,
+    });
+
+    const mutation = useMutation({
+        mutationFn: createExpenses,
+        onSuccess: (data) => {
+            const messages = data.data.messages || "tạo thành công!";
+            toast.success(messages);
+            reset();
+        },
+        onError: (error) => {
+            console.error("Error posting product:", error);
+            toast.error("Tạo budget thất bại");
+        },
+    });
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const handleCreateExpenses = async (data: any) => {
+        mutation.mutate(data);
     };
+
+    if (isPending) {
+        return "...loading!";
+    }
+
+    const cateExpenseDefaultSelect =
+        cateExpenseData.data.categoriesExpenes[0]._id;
+
     return (
         <>
             <Grid columnNumber={1} gap="18px">
-                <form onSubmit={handleSubmit(handleCreateTypeBudgets)}>
+                <form onSubmit={handleSubmit(handleCreateExpenses)}>
                     <Grid gap="18px" columnNumber={3}>
                         <div className="form-flex-cl">
                             <label htmlFor="">Budgets type option</label>
                             <SelectBox
                                 control={control}
-                                options={_budgetsTypeOption}
-                                selectValue={budgetsOption[0].value}
-                                name={"budgetsOption"}
+                                options={cateExpenseData.data.categoriesExpenes}
+                                selectValue={cateExpenseDefaultSelect}
+                                name={"id_expense_cate"}
                             />
+
+                            {errors.id_expense_cate && (
+                                <ErrorMess>
+                                    {errors.id_expense_cate.message}
+                                </ErrorMess>
+                            )}
                         </div>
                         <div className="form-flex-cl">
                             <label htmlFor="">Amount budgets</label>
@@ -100,30 +126,42 @@ const FormExpense = () => {
                                 refinput={register("amount")}
                                 placeholder="20.000.000 vnd"
                             />
+                            {errors.amount && (
+                                <ErrorMess>{errors.amount.message}</ErrorMess>
+                            )}
                         </div>
                         <div className="form-flex-cl">
                             <label htmlFor="">Currency option</label>
                             <SelectBox
                                 control={control}
                                 options={currencyOption}
-                                selectValue={_currencyOption[0].value}
-                                name={"currency"}
+                                selectValue={"ss"}
+                                name={"currency_code"}
                             />
+                            {errors.currency_code && (
+                                <ErrorMess>
+                                    {errors.currency_code.message}
+                                </ErrorMess>
+                            )}
                         </div>
                         <div className="form-flex-cl">
                             <label htmlFor="">Duration</label>
                             <InputDate
                                 control={control}
-                                name="date-duration"
+                                name="duration"
                             ></InputDate>
+                            {errors.duration && (
+                                <ErrorMess>{errors.duration.message}</ErrorMess>
+                            )}
                         </div>
                         <div className="form-flex-cl">
                             <label htmlFor="">Paid</label>
                             <Grid gap="18px" columnNumber={2}>
                                 <div>
                                     <InputRadio
-                                        refinput={register("description")}
+                                        refinput={register("is_paid")}
                                         placeholder="description expense month"
+                                        value="true"
                                     />
                                     <label
                                         htmlFor=""
@@ -134,8 +172,9 @@ const FormExpense = () => {
                                 </div>
                                 <div>
                                     <InputRadio
-                                        refinput={register("description")}
+                                        refinput={register("is_paid")}
                                         placeholder="description expense month"
+                                        value="false"
                                     />
                                     <label
                                         htmlFor=""
@@ -152,6 +191,11 @@ const FormExpense = () => {
                                 refinput={register("description")}
                                 placeholder="description expense month"
                             />
+                            {errors.description && (
+                                <ErrorMess>
+                                    {errors.description.message}
+                                </ErrorMess>
+                            )}
                         </div>
                     </Grid>
                     <Button
