c12762a56e6a026009e0a428b83fd5df464baa18
LongNguyen25012002 <25nguyenduclong@gmail.com>
2024-07-26 22:34:54 +0700

    dispatch user user for global

--- frontend/src/routers/privateRouter/PrivateRouter.tsx
+++ frontend/src/routers/privateRouter/PrivateRouter.tsx
@@ -1,8 +1,11 @@
 import { FC, ReactNode, useEffect } from "react";
 import { Navigate } from "react-router-dom";
-import { useAppDispatch, useAppSelector } from "@/hooks";
-import { authSelector, fetchUsers } from "@/store/authSlice/authSlice";
+import { useAppDispatch } from "@/hooks";
+import { getUserInfor } from "@/store/authSlice/authSlice";
 import Cookies from "js-cookie";
+import { getUserWithToken } from "@/api/userApi";
+import { useQuery } from "@tanstack/react-query";
+import { toast } from "react-toastify";
 
 interface PrivateRouterProps {
     children: ReactNode;
@@ -10,24 +13,31 @@ interface PrivateRouterProps {
 
 const PrivateRouter: FC<PrivateRouterProps> = ({ children }) => {
     const dispatch = useAppDispatch();
-
-    const { user, loading, error } = useAppSelector(authSelector);
-    console.log(user);
-
     const accessToken = Cookies.get("jwt");
 
-    useEffect(() => {
-        if (accessToken) {
-            dispatch(fetchUsers());
-        }
-    }, [dispatch, accessToken]);
+    const { data, error, isLoading, isError, isSuccess } = useQuery({
+        queryKey: ["userInfo"],
+        queryFn: getUserWithToken,
+        enabled: !!accessToken,
+        staleTime: Infinity,
+    });
 
-    if (loading) {
+    useEffect(() => {
+        if (isError) {
+            toast.error(`login error ${error.message}`);
+        }
+        if (isSuccess) {
+            dispatch(getUserInfor(data));
+        }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [dispatch, data, isSuccess, isError]);
+
+    if (isLoading) {
         return <div>Loading...</div>;
     }
 
-    if (error === "failed") {
-        return <div>Error: {error}</div>;
+    if (isError) {
+        return <div>Error: {error.message}</div>;
     }
 
     if (!accessToken) {
