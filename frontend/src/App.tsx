ce18063494b453818454a5cde82d42324d8438c7
LongNguyen25012002 <25nguyenduclong@gmail.com>
2024-07-26 22:28:09 +0700

    add component ToastContainer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,8 @@ import router from "./routers/routers";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const queryClient = new QueryClient();
 
@@ -14,6 +16,10 @@ const App = () => {
                 <QueryClientProvider client={queryClient}>
                     <GlobalStyles>
                         <RouterProvider router={router} />
+                        <ToastContainer
+                            autoClose={5000}
+                            position="bottom-right"
+                        />
                     </GlobalStyles>
                 </QueryClientProvider>
             </Provider>
