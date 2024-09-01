3e9b403e4fa31719189c0f994f931bbd84e28724
LongNguyen25012002 <25nguyenduclong@gmail.com>
2024-09-01 23:47:33 +0700

    add services auh api

--- frontend/src/api/authApi.ts
+++ frontend/src/api/authApi.ts
@@ -4,12 +4,30 @@ import { AuthGuard } from "./models/authGuard";
 const AUTH_URL: string = "http://localhost:3000/api/v1/auth";
 const URL_LOGIN_GOOGLE: string = AUTH_URL + "/google";
 
-const reigster = (userPayload: AuthGuard) => {
-    return apiBase.post(`${AUTH_URL}/register`, userPayload);
+const reigster = async (userPayload: AuthGuard) => {
+    try {
+        return apiBase.post(`${AUTH_URL}/register`, userPayload);
+    } catch (error) {
+        console.log(error);
+    }
 };
 
-const login = (userPayload: AuthGuard) => {
-    return apiBase.post(`${AUTH_URL}/login`, userPayload);
+const login = async (userPayload: AuthGuard) => {
+    try {
+        return apiBase.post(`${AUTH_URL}/login`, userPayload);
+    } catch (error) {
+        console.log(error);
+    }
 };
 
-export { reigster, login, URL_LOGIN_GOOGLE };
+const logout = async () => {
+    try {
+        return apiBase
+            .post(`${AUTH_URL}/logout`)
+            .then((result) => result.status);
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export { reigster, login, logout, URL_LOGIN_GOOGLE };
