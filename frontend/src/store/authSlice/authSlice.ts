4891c53917a432f892a96b7126062802e0e3b381
LongNguyen25012002 <25nguyenduclong@gmail.com>
2024-09-01 23:47:54 +0700

    update state in auth reducer

--- frontend/src/store/authSlice/authSlice.ts
+++ frontend/src/store/authSlice/authSlice.ts
@@ -1,32 +1,44 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import type { PayloadAction } from "@reduxjs/toolkit";
 import { RootState } from "../store";
+import { getUserWithToken } from "@/api/userApi";
 
 interface UserState {
-    name: string;
+    username: string;
     email: string;
     role: string;
     avatar: string;
     _id: string;
+    emailVerify: boolean;
+    provider: string;
 }
 
+const userState = {
+    username: "",
+    email: "",
+    role: "",
+    avatar: "",
+    _id: "",
+    provider: "",
+    emailVerify: false,
+};
 export interface AuthSliceState {
-    user: UserState | null;
+    user: UserState;
     loading: boolean;
     error: string | undefined;
+    isAuthenticated: boolean;
 }
 
 const initialState: AuthSliceState = {
-    user: null,
+    user: userState,
     loading: false,
     error: "",
+    isAuthenticated: false,
 };
 
 export const fetchUsers = createAsyncThunk("auth/fetchUsers", async () => {
-    const response = await fetch(
-        `https://jsonplaceholder.typicode.com/users?id=1`
-    );
-    return await response.json();
+    const userInfor = await getUserWithToken();
+    return userInfor;
 });
 
 export const authSlice = createSlice({
@@ -42,17 +54,22 @@ export const authSlice = createSlice({
         });
         builder.addCase(fetchUsers.rejected, (state, action) => {
             state.loading = false;
-            state.user = null;
+            state.user = userState;
             state.error = action.error.message;
         });
     },
     reducers: {
         getUserInfor: (state, actions: PayloadAction<UserState>) => {
             state.user = actions.payload;
+            state.isAuthenticated = true;
+        },
+        logoutSuccess: (state) => {
+            state.user = userState;
+            state.isAuthenticated = false;
         },
     },
 });
 
-export const { getUserInfor } = authSlice.actions;
+export const { getUserInfor, logoutSuccess } = authSlice.actions;
 export const authSelector = (state: RootState) => state.auth;
 export default authSlice.reducer;
