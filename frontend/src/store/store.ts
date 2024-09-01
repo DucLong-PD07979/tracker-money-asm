39578c7a760591972afad07b2523fca747b39a26
LongNguyen25012002 <25nguyenduclong@gmail.com>
2024-09-01 23:44:25 +0700

    update new reducer in store

--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -1,15 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./authSlice/authSlice";
 import uiReducer from "./uiSlice/uiSlice";
+import budgetReducer from "./budgetSlice/budgetSlice";
+import expenseReducer from "./expenseSlice/expenseSlice";
 
 export const store = configureStore({
     reducer: {
         auth: authReducer,
         ui: uiReducer,
+        budget: budgetReducer,
+        expense: expenseReducer,
     },
 });
 
-// Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
