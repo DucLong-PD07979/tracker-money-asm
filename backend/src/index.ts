c525d47343ec7aa5b93bf17de7db36fed5d64a07
LongNguyen25012002 <25nguyenduclong@gmail.com>
2024-09-11 15:59:09 +0700

    update code for deploy

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,7 +2,6 @@ import app from './server';
 import { PORT_SERVER } from './configs/environment';
 import passportConfig from '@/configs/passports';
 const PORT = PORT_SERVER || 3000;
-passportConfig;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
