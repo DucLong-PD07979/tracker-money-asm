8217697a9011efedb6bebb1dd1998873f3aae482
LongNguyen25012002 <25nguyenduclong@gmail.com>
2024-09-11 15:41:53 +0700

    remove

--- backend/src/routers/api/v1/budgetCateRouters.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { createBudgetCate, getBudgetCateAll } from '@/controllers/budgetCateController';
-
-const Router = express.Router();
-
-Router.post('/budgets-categories', createBudgetCate);
-Router.get('/budgets-categories', getBudgetCateAll);
-
-export default Router;
--- backend/src/routers/api/v1/categoriesExpenseRouter.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { createCategories, getAllCategories } from '@/controllers/categorieExpenseController';
-
-const Router = express.Router();
-
-Router.post('/categories', createCategories);
-Router.get('/categories', getAllCategories);
-
-export default Router;
